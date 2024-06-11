d268184f440a505131531de817b6d8d6912364f0
Vitalii Vorobii <vitalii.vorobii@intellias.com>
2024-06-11 17:08:28 +0100

    add skeleton

---
+++ positions-service/src/main/java/org/citrus/learn/positionsservice/PositionsServiceApplication.java
@@ -0,0 +1,13 @@
+package org.citrus.learn.positionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionsServiceApplication.class, args);
+	}
+
+}
